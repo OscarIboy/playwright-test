7302cdfe3d23cd011acbd1846c61386f44269eaf
Oscar <oscar@r-digital.tech>
2023-05-10 03:25:21 -0600

    update tests

--- tests/assert.spec.ts
+++ tests/assert.spec.ts
@@ -1,14 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('playing with assertions', async ({ page }) => {
+
+    //Go to
     await page.goto('http://uitestingplayground.com/textinput');
 
     //verify input is visible
-    await expect(page.locator('#newButtonName')).toBeVisible;
+    await expect(page.locator('#newButtonName')).toBeVisible();
+
     //select input and fill the input your text
     await page.locator('#newButtonName').fill('Oscar');
+
+    //pause in dev local
+    await page.pause();
+
     //click in button
     await page.locator('#updatingButton').click();
+
     //verify button text updated
     await expect(page.locator('#updatingButton')).toContainText('Oscar');
 });
--- tests/tiendaonline.spec.ts
+++ tests/tiendaonline.spec.ts
@@ -6,7 +6,7 @@ test('Challenge 1', async ({ page }) => {
     await page.goto('https://automationexercise.com/');
 
     //Click on "View Product" of the first product
-    await expect(page.locator('a:has-text("View Product")')).toBeVisible;
+    await page.hover('.choose > .nav > li > a');
     await page.locator('a:has-text("View Product")').first().click();
     
     //Close Add
@@ -17,16 +17,16 @@ test('Challenge 1', async ({ page }) => {
     await page.locator('#quantity').fill('3');
 
     //Click on "Add to cart"
-    await expect(page.locator('button:has-text("Add to cart")')).toBeVisible;
+    await expect(page.locator('button:has-text("Add to cart")')).toBeVisible();
     await page.locator('button:has-text("Add to cart")').click();
 
     //Verify that the modal and the text of "Added!" is visible
-    await expect(page.locator('modal-title:has-text("Added!")')).toBeVisible;
+    await expect(page.locator('modal-title:has-text("Added!")')).toBeVisible();
 
     //Click on the button of "Continue Shopping"
-    await expect(page.locator('button:has-text("Continue Shopping")')).toBeVisible;
+    await expect(page.locator('button:has-text("Continue Shopping")')).toBeVisible();
     await page.locator('button:has-text("Continue Shopping")').click();
 
     //Verify that the modal is not visible modal-content
-    await expect(page.locator('modal-content')).toBeHidden;
+    await expect(page.locator('modal-content')).not.toBeVisible();
 });
